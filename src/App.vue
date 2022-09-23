c6e781696f55838a419700ccb684a83c39a8adcf
matheusavizsenacm <matheus.aviz@alunos.sc.senac.br>
2022-09-23 20:46:11 -0300

    AtividadeAvaliacaoNota

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <label for="nomeAluno">Insira o nome do Aluno: </label>
+  <input id="nomeAluno" v-model="nomeAluno" type="text"><br>
+  <label for="nota1">Insira a primeira nota: </label>
+  <input id="nota1" v-model="nota1" type="number"><br>
+  <label for="nota2">Insira a segunda nota: </label>
+  <input id="nota2" v-model="nota2" type="number"><br>
+  <label for="nota3">Insira a terceira nota: </label>
+  <input id="nota3" v-model="nota3" type="number"><br>
+  <label for="nota3">Insira a media: </label>
+  <input id="media" v-model="media" type="number"><br>
+  <Nota-Aluno :nomeAluno="nomeAluno" :nota1="nota1" :nota2="nota2" :nota3="nota3" :media="media"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NotaAluno from './components/NotaAluno.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NotaAluno
+  },
+
+  data(){
+    return{
+      nomeAluno: "",
+      nota1: 0,
+      nota2: 0,
+      nota3: 0,
+      media: 0
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/NotaAluno.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+      <input type="button" value="Calcular" @click="calculaNota"/><br/><br/>
+      <br>
+      <table>
+        <tr>
+            <th>Nome do Aluno</th>
+            <th>Situacao</th>
+        </tr>
+        <tr v-for='item in listaAlunos' :key="item.NomeAluno">
+            <td v-if="item.situacao == 'Aprovado'" style="background-color: blue;">
+                {{item.nome}}
+            </td>
+            <td v-if="item.situacao == 'Aprovado'" style="background-color: blue;">
+              {{item.situacao}}
+            </td>
+            <td v-if="item.situacao == 'Reprovado'" style="background-color: Red;">
+                {{item.nome}}
+            </td>
+            <td v-if="item.situacao == 'Reprovado'" style="background-color: Red;">
+              {{item.situacao}}
+            </td>
+        </tr>
+
+    </table>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: "Nota-Aluno",
+    props: {
+      nomeAluno: String, 
+      nota1: Number,
+      nota2: Number,
+      nota3: Number,
+      media: Number,
+    },
+  
+    data(){
+      return {
+        listaAlunos: []
+      }
+      },
+
+    methods: {
+      calculaNota(){
+        var situacao = ""
+        var notaFinal = (this.nota1 + this.nota2 * 2 + this.nota3 * 3 + this.media ) / 7
+        if (notaFinal >= 7){
+          situacao = "Aprovado"
+        } else{situacao = "Reprovado"}
+        
+        this.adicionaLista(this.nomeAluno, situacao)
+      },
+      adicionaLista(nome, situacao){
+        this.listaAlunos.push({nome: nome, situacao: situacao})
+      }
+    }
+
+    }
+  
+  
+  </script>
+  
+
+  <style scoped>
+  table, th, td {
+    border: 1px solid;
+  }
+  
+  </style>
+  
